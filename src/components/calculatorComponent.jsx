e34d2a905c0b844b9d3fe8737cb4204cede84cd9
15sora2 <stgordon19@gmail.com>
2019-09-17 18:07:14 -0500

    created number buttons that change the number property

--- src/components/calculatorComponent.jsx
+++ src/components/calculatorComponent.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 
 class Calculator extends Component {
-  state = {};
+  state = {
+    number: 0
+  };
+
   render() {
-    return <div>There will soon be a calculator here!</div>;
+    return (
+      <div>
+        <span>{this.state.number}</span>
+        <button onClick={() => this.setState({ number: 1 })}>1</button>
+        <button onClick={() => this.setState({ number: 2 })}>2</button>
+        <button onClick={() => this.setState({ number: 3 })}>3</button>
+        <button onClick={() => this.setState({ number: 4 })}>4</button>
+        <button onClick={() => this.setState({ number: 5 })}>5</button>
+        <button onClick={() => this.setState({ number: 6 })}>6</button>
+        <button onClick={() => this.setState({ number: 7 })}>7</button>
+        <button onClick={() => this.setState({ number: 8 })}>8</button>
+        <button onClick={() => this.setState({ number: 9 })}>9</button>
+        <button onClick={() => this.setState({ number: 0 })}>0</button>
+        <button>=</button>
+      </div>
+    );
   }
 }
 
