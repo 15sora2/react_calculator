acbf3ac2dd85503b9c8cf9ae5c2aa787db5a4fad
15sora2 <stgordon19@gmail.com>
2019-09-17 16:32:35 -0500

    created calculator component

---
+++ src/components/calculatorComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  state = {};
+  render() {
+    return <h1>There will be a calculator here at some point!</h1>;
+  }
+}
+
+export default Calculator;
